74b19d865367aeff6af754c704d5da6ea6929441
Deguzis <mtdeguzis@geisinger.edu>
2018-03-10 09:55:33 -0500

    detect tmux ver

--- setup.sh
+++ setup.sh
@@ -22,26 +22,38 @@ done
 #	symlink the last file to the desired save file: $ ln -sf <file_name> last
 #	do a restore with tmux-resurrect key: prefix + Ctrl-r
 echo -e "\n==> Extra tmux setup\n"
-if [[ ! -d "${HOME}/tmux-resurrect" ]]; then
-	echo "Missing tmux-resurrect, cloning"
-	git clone https://github.com/tmux-plugins/tmux-resurrect ~/tmux-resurrect
-else
-	echo "updating tmux-ressurrect"
-	cd "${HOME}/tmux-resurrect"
-	git pull
-	cd "${scriptdir}"
-fi
 
-if [[ ! -d "${HOME}/tmux-continuum" ]]; then
-	echo "Missing tmux-continuum, cloning"
-	git clone https://github.com/tmux-plugins/tmux-continuum ~/tmux-continuum
-else
-	echo "updating /tmux-continuum"
-	cd "${HOME}//tmux-continuum"
-	git pull
-	cd "${scriptdir}"
-fi
+# only install this if tmux > 1.9
+# Check for bad exit status on this eval
+if [[ ! $(echo "$(tmux -V | sed 's/tmux //') < 1.9" | bc -l) ]]; then
 
-# reload tmux conf
-echo "reloading tmux conf"
-tmux source-file ~/.tmux.conf
+	if [[ ! -d "${HOME}/tmux-resurrect" ]]; then
+		echo "Missing tmux-resurrect, cloning"
+		git clone https://github.com/tmux-plugins/tmux-resurrect ~/tmux-resurrect
+	else
+		echo "updating tmux-ressurrect"
+		cd "${HOME}/tmux-resurrect"
+		git pull
+		cd "${scriptdir}"
+	fi
+
+	if [[ ! -d "${HOME}/tmux-continuum" ]]; then
+		echo "Missing tmux-continuum, cloning"
+		git clone https://github.com/tmux-plugins/tmux-continuum ~/tmux-continuum
+	else
+		echo "updating /tmux-continuum"
+		cd "${HOME}//tmux-continuum"
+		git pull
+		cd "${scriptdir}"
+	fi
+
+	# reload tmux conf
+	echo "reloading tmux conf"
+	tmux source-file ~/.tmux.conf
+
+else
+
+	echo "$(tmux -V) is too old for tmux-continuum"
+	echo "Please use bin/tmux-ressurect instead (see README.md in bin/)"
+
+fi
